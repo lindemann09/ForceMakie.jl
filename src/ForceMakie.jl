7f0619d4a14fb418e16cf7dbd599bf0589b09944
Oliver Lindemann <lindemann09@googlemail.com>
2023-06-29 09:52:55 +0200

    v0.1.2

--- src/ForceMakie.jl
+++ src/ForceMakie.jl
@@ -11,12 +11,12 @@ function plot_good_bad(fig::Figure,
                     force::Matrix;
                     zero_sample::Int = 0,
                     ylims::UnitRange{Int}=-2000:2000,
-                    good_trials::AbstractVector{Bool}=[true],
+                    good_trials::Union{Nothing, AbstractVector{Bool}}=nothing,
                     colors_good = RGBAf(0.2, 0.6, 0.2, 0.5),
                     color_bad = RGBAf(0.9, 0.4, 0.4, 0.5),
-                    marker::AbstractVector{Int} = [],
-                    info_text::AbstractString = "" )
-
+                    marker::Union{Nothing, AbstractVector{Int}} = nothing,
+                    linewidth::Int=2,
+                    info_text::AbstractString = "")
     plot_good_bad(Axis(fig[1, 1]),
                     force;
                     zero_sample,
@@ -25,26 +25,29 @@ function plot_good_bad(fig::Figure,
                     colors_good ,
                     color_bad ,
                     marker,
+                    linewidth,
                     info_text)
 
 end
+
 function plot_good_bad(ax::Axis,
                 force::Matrix;
                 zero_sample::Int = 0,
                 ylims::UnitRange{Int}=-2000:2000,
-                good_trials::AbstractVector{Bool}=[true],
+                good_trials::Union{Nothing, AbstractVector{Bool}}=nothing,
                 colors_good = RGBAf(0.2, 0.6, 0.2, 0.5),
                 color_bad = RGBAf(0.9, 0.4, 0.4, 0.5),
-                marker::AbstractVector{Int} = [],
-                info_text::AbstractString = "" )
+                marker::Union{Nothing, AbstractVector{Int}} = nothing,
+                linewidth::Int=2,
+                info_text::AbstractString = "")
     nrow = size(force, 1)
-    if length(good_trials) != nrow
+    if isnothing(good_trials)
         # define sample range based on zero_sample
         good_trials = repeat([true], nrow)
     end;
 
     xs = (1-zero_sample):(size(force, 2)-zero_sample)
-    if length(marker) > 0
+    if !isnothing(marker)
         vlines!(ax, marker, linewidth = 0.5, color=:gray)
     end
     for i in 1:nrow
@@ -53,7 +56,7 @@ function plot_good_bad(ax::Axis,
         else
             col = color_bad
         end
-        lines!(xs, force[i, :], color=col)
+        lines!(xs, force[i, :], color=col, linewidth=linewidth)
     end
     ylims!(ax, ylims.start, ylims.stop)
     text!(ax, 0, 1,
@@ -65,10 +68,11 @@ end;
 
 
 function plot_av_profile(fig::Figure, fp::ForceProfiles;
-        dv::Symbol = :all,
+        group::Symbol = :all,
         colorscheme::ColorScheme = ColorSchemes.tableau_20,
         color_steps=1/8,
-        marker = Int64[])
+        marker = Int64[],
+        errors=true)
     # conditions is a variable with the conditions
     # has to have the same number of elemens as rows in froce
 
@@ -78,13 +82,13 @@ function plot_av_profile(fig::Figure, fp::ForceProfiles;
     if length(marker) > 0
         vlines!(ax, marker, linewidth = 1.5, color=:gray)
     end
-    if dv == :all
+    if group == :all
         # no conditions defined
         cond_dict = Dict("all" => 1:size(force, 1))
     else
         cond_dict = Dict()
-        for c in unique(fp.design[:, dv])
-            cond_dict[string(c)] = findall(fp.design[:, dv].==c)
+        for c in unique(fp.design[:, group])
+            cond_dict[string(c)] = findall(fp.design[:, group].==c)
         end
     end
 
@@ -95,13 +99,13 @@ function plot_av_profile(fig::Figure, fp::ForceProfiles;
         val = column_mean(force, rows=cond_dict[k])
         lines!(xs, val, label = k, linewidth=5, color = cols[i])
 
-        err = column_std(force, rows=cond_dict[k]) / sqrt(length(cond_dict[k]))
-        band!(xs, val+err, val-err, color = (cols[i], 0.2))
+        if errors
+            err = column_std(force, rows=cond_dict[k]) / sqrt(length(cond_dict[k]))
+            band!(xs, val+err, val-err, color = (cols[i], 0.2))
+        end
     end
     return fig, ax
 end;
-
-
 
 
 
